212bbb65aefb1334f9b912fc8a179c9d996371b0
qilei <qilei@threatbook.cn>
2025-05-06 11:49:51 +0800

    feat: api merge

--- custom.js
+++ custom.js
@@ -1,8 +1,3 @@
-// 查找导航的contact us按钮，并添加指定的class
-const contactBtn = document.querySelector('a[href="https://i.secai.ai/contact_us"]');
-
-contactBtn.classList.add('hs-cta-trigger-button', 'hs-cta-trigger-button-189188848506');
-
 const downloadUrl = {
   download_ioc: '/images/SECAI_IOC_Sample.json',
   download_ip: '/images/SECAI_IP_Reputation_Sample.json'
@@ -15,18 +10,6 @@ function downloadFile(id) {
   a.download = url.split('/').pop();
   a.click();
 }
-
-fetch('https://js.hs-scripts.com/48609194.js')
-.then(response => response.text())
-.then(scriptContent => {
-  const script = document.createElement('script');
-  script.textContent = scriptContent;
-  script.id = 'hs-script-loader';
-  script.async = true;
-  script.defer = true;
-  document.body.appendChild(script);
-})
-.catch(error => console.error('加载HubSpot脚本失败:', error));
 
 window.addEventListener('load', () => {
   document.addEventListener('click', e => {
