fd8b03a5d64f8c1faf2d112069b265c0ad6c59e6
qilei <qilei@threatbook.cn>
2025-05-29 09:44:38 +0800

    feat: init

---
+++ custom.js
@@ -0,0 +1,22 @@
+const downloadUrl = {
+  download_ioc: '/images/SECAI_IOC_Sample.json',
+  download_ip: '/images/SECAI_IP_Reputation_Sample.json'
+}
+
+function downloadFile(id) {
+  const url = downloadUrl[id];
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = url.split('/').pop();
+  a.click();
+}
+
+window.addEventListener('load', () => {
+  document.addEventListener('click', e => {
+    let div = e.target.closest('div');
+    if (e.target.id === 'download_ioc' || e.target.id === 'download_ip' || div.id === 'download_ioc' || div.id === 'download_ip') {
+      const id = e.target.id || div.id;
+      downloadFile(id);
+    }
+  })
+})
